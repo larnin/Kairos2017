27e0a592d38165f4eaa5c1c654eef848b8105e73
Nicolas <nicolas-donald.laurent@laposte.net>
2017-11-14 15:39:01 +0100

    Add tool for story book

---
+++ Assets/G.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public sealed class G
+{
+    private static volatile G m_instance;
+    private InventoryData m_inventory = new InventoryData();
+
+    public static G sys
+    {
+        get
+        {
+            if (G.m_instance == null)
+                G.m_instance = new G();
+            return G.m_instance;
+        }
+    }
+
+    public InventoryData inventory { get { return m_inventory; } }
+}
---
+++ Assets/Inventory/InventoryData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class CardData
+{
+    public CardData(string _name, string _description)
+    {
+        name = _name;
+        description = _description;
+    }
+
+    public string name;
+    public string description;
+}
+
+public class StoryData
+{
+
+}
+
+
+public class InventoryData
+{
+    private List<CardData> m_cards;
+    private List<StoryData> m_story;
+
+
+}
