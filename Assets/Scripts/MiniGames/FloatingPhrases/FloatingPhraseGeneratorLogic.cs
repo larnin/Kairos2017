2baa803a8ecfa514d7ee0eeca756c4b784385422
Julien <juliensud@live.fr>
2017-11-23 11:05:40 +0100

    Mostly clean rumorSHadowMiniGame (firstVersion)

--- Assets/Scripts/MiniGames/FloatingPhrases/FloatingPhraseGeneratorLogic.cs
+++ Assets/Scripts/MiniGames/FloatingPhrases/FloatingPhraseGeneratorLogic.cs
@@ -8,15 +8,12 @@ using UnityEngine;
  * */
 public class FloatingPhraseGeneratorLogic : MonoBehaviour
 {
-    [SerializeField]
-    private List<Transform> m_MultipleSpawnEmplacement;
-    private List<Transform> m_ReserveMultipleSpawnEmplacement;
-    private int MultipleSpawnEmplacementIndex = 0;
-
-    [SerializeField]
-    private List<Transform> m_MultipleRestEmplacement;
-    private List<Transform> m_ReserveMultipleRestEmplacement;
-    private int MultipleRestEmplacementIndex = 0;
+    [System.Serializable]
+    public class PairPhrases
+    {
+        public FloatingPhraseLogic m_floatingPhrasePrebabs;
+        public FloatingPhraseLogic B;
+    }
 
     [SerializeField]
     private float m_minDelayToSpawn = 0.5f;
@@ -39,7 +36,6 @@ public class FloatingPhraseGeneratorLogic : MonoBehaviour
      * note : lorsque une phrase est détruit elle est ajoute dans la reserve temporaire (m_tempReserveFloatingPhrase) 
      * Si la reserve est vide meme aprés l'avoir rempli avec la reserve temporaire, on ne fait rien) 
      */
-
     private List<byte> m_reserveFloatingPhrase = new List<byte>();
     private List<byte> m_tempReserveFloatingPhrase = new List<byte>();
     private List<FloatingPhraseLogic> m_spawnedFloatingPhrase = new List<FloatingPhraseLogic>();
@@ -62,17 +58,6 @@ public class FloatingPhraseGeneratorLogic : MonoBehaviour
             m_reserveFloatingPhrase.Add(i);
         }
         
-
-        // code de test !
-        //SpawnFloatingPhrase();
-        /*
-        if (m_floatingPhrasePrebabs.Length > 0)
-        {
-            for(int i = 0; i < m_floatingPhrasePrebabs.Length ; i++)
-            {
-                Invoke("TrySpawnFloatingPhrase", 2f + i * 0.1f);
-            }
-        }*/
         InvokeRepeating("TrySpawnFloatingPhrase", m_minDelayToSpawn, m_minDelayToSpawn);
     }
     
@@ -98,31 +83,11 @@ public class FloatingPhraseGeneratorLogic : MonoBehaviour
         byte index = (byte)m_reserveFloatingPhrase[UnityEngine.Random.Range(0, m_reserveFloatingPhrase.Count)];
 
         Vector3 positionForSpawn = Vector3.zero;
-
-        if(m_MultipleSpawnEmplacement.Count > 0)
-        {
-            positionForSpawn = m_MultipleSpawnEmplacement[MultipleSpawnEmplacementIndex % m_MultipleSpawnEmplacement.Count].position;
-            MultipleSpawnEmplacementIndex++;
-        }
-        else
-        {
-            positionForSpawn = m_emplacementForSpawn.position;
-        }
-
+        positionForSpawn = m_emplacementForSpawn.position;
+        
         FloatingPhraseLogic spawned = Instantiate(m_floatingPhrasePrebabs[index], positionForSpawn, Quaternion.identity);
-
-        if (m_MultipleRestEmplacement.Count > 0)
-        {
-            spawned.SetTargetToRest(m_MultipleRestEmplacement[MultipleRestEmplacementIndex % m_MultipleRestEmplacement.Count]);
-            MultipleRestEmplacementIndex++;
-        }
-        else
-        {
-            spawned.SetTargetToRest(m_targetToRest);
-        }
+        spawned.SetTargetToRest(m_targetToRest);
         
-        
-        //spawned.SetDecalSin(m_decalSin);
         spawned.Index = index;
         spawned.onDestroyCallback += OnPhraseIsDestroy;
         spawned.onSelected += m_rumorsOfShadowsManager.floatingPhraseIsSelected;
