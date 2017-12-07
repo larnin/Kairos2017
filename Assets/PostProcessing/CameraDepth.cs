8793f72761d1c33d2633bd194a5406a6f69912d0
Paul <p.freland@rubika-edu.com>
2017-12-07 11:01:56 +0100

    World building

---
+++ Assets/PostProcessing/CameraDepth.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDepth : MonoBehaviour {
+
+	Camera AttachedCamera;
+	public RenderTexture depthMap;
+
+	void Start () 
+	{
+		AttachedCamera = GetComponent<Camera>();
+		AttachedCamera.depthTextureMode = DepthTextureMode.DepthNormals;
+		depthMap.format = RenderTextureFormat.Depth;
+	}
+
+	void OnRenderImage(RenderTexture source, RenderTexture destination)
+	{
+		
+	}
+}
